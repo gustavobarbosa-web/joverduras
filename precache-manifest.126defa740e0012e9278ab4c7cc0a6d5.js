d0fd616868237f0f2f05d6e7dd28591fa7d874a7
Gustavo Barbosa <gustavobarbosa.contato@gmail.com>
2019-06-08 21:59:23 -0300

    Updates

---
+++ precache-manifest.126defa740e0012e9278ab4c7cc0a6d5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1819da559ad77973be5d3817cd2188a8",
+    "url": "/joverduras/index.html"
+  },
+  {
+    "revision": "b5e94bedf0266cc04189",
+    "url": "/joverduras/static/css/2.e7851710.chunk.css"
+  },
+  {
+    "revision": "bb1e4ee9ef8b9e6c6f7c",
+    "url": "/joverduras/static/css/main.3294b078.chunk.css"
+  },
+  {
+    "revision": "b5e94bedf0266cc04189",
+    "url": "/joverduras/static/js/2.6ea9d92a.chunk.js"
+  },
+  {
+    "revision": "bb1e4ee9ef8b9e6c6f7c",
+    "url": "/joverduras/static/js/main.4564f872.chunk.js"
+  },
+  {
+    "revision": "1865cfb10e77e3a1bb8d",
+    "url": "/joverduras/static/js/runtime~main.c18cb5d5.js"
+  },
+  {
+    "revision": "3305f866538b0017b995ebd043ff5d71",
+    "url": "/joverduras/static/media/banner1.3305f866.jpg"
+  },
+  {
+    "revision": "70884ac2b568425dea42d6f0427f288a",
+    "url": "/joverduras/static/media/banner2.70884ac2.jpg"
+  },
+  {
+    "revision": "022073b20761f299a1c6b83f03fc1136",
+    "url": "/joverduras/static/media/banner3.022073b2.jpg"
+  },
+  {
+    "revision": "3d33d101e6cccc6a3a0939c3415dd5a1",
+    "url": "/joverduras/static/media/img1.3d33d101.jpg"
+  },
+  {
+    "revision": "1667a67d2691341f9871af423ee0a9e2",
+    "url": "/joverduras/static/media/img4.1667a67d.jpg"
+  },
+  {
+    "revision": "f15c577b60af74723c7ec08256949ff8",
+    "url": "/joverduras/static/media/logow.f15c577b.png"
+  },
+  {
+    "revision": "0ef5dd6962f9a8acc4ff6c423b471f3c",
+    "url": "/joverduras/static/media/separator.0ef5dd69.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/joverduras/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/joverduras/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/joverduras/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/joverduras/static/media/slick.f97e3bbf.svg"
+  }
+]);
